0c421fb5a42cc90789f5284a9f300a82873ea694
timurnurutdinov <timur.nurutdinov@flatstack.com>
2015-08-28 22:41:58 +0300

    Cancelled
    NSModal blocks next code. Got rid of UI pre detection.

--- Framer Inventory.sketchplugin/Contents/Sketch/framer-inventory-text.js
+++ Framer Inventory.sketchplugin/Contents/Sketch/framer-inventory-text.js
@@ -1,3 +1,10 @@
 var inventorySuccessMessage = "Copied to Clipboard. ⌘CMD+V into Framer Studio"
 var inventoryNoLayersSelectedMessage = "Failed. Select one or more layers, groups or slices, please."
-var inventoryNoExportablesForKeynote = "Nothing will be exported. Make exportable\rgroups or create slices on this page"
+var inventoryNoExportablesForKeynote = "Nothing has been exported. Make exportable groups or create slices on this page."
+
+
+function inventorySuccessMessageNumberedGetter(localNumber) {
+	if (localNumber == nil || localNumber == 0) { return inventoryNoExportablesForKeynote }
+	else if (localNumber == 1) { return "FramerJS prototype with one layer was copied to Clipboard. ⌘CMD+V into Framer Studio." }
+	return "FramerJS prototype with " + localNumber + " layers was copied to Clipboard. ⌘CMD+V into Framer Studio."
+}
