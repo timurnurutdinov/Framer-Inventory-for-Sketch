0d471accf84e2dcb82757c6db7dfee3d76262cf6
Timur Nurutdinov <addleimb@gmail.com>
2016-12-23 11:30:48 +0300

    Updated UI to be "like Sketch"

--- Framer Inventory.sketchplugin/Contents/Sketch/lib.js
+++ Framer Inventory.sketchplugin/Contents/Sketch/lib.js
@@ -1,4 +1,5 @@
 var pathLabel = nil
+var defaultPathLabel = "Please, select Framer folder"
 
 function ToolbarInventory () {}
 
@@ -451,7 +452,7 @@ ToolbarInventory.updateContext = function() {
 ToolbarInventory.getImage = function(size, name) {
     var isRetinaDisplay = (NSScreen.mainScreen().backingScaleFactor() > 1)? true: false;
     var suffix = (isRetinaDisplay)? "@2x": "";
-    var imageURL = NSURL.fileURLWithPath(pluginPath + "/" + "/images" + "/uibar/" + name + suffix + ".png");
+    var imageURL = NSURL.fileURLWithPath(pluginPath + "/" + "/images" + "/toolbar/" + name + suffix + ".png");
     var image = NSImage.alloc().initWithContentsOfURL(imageURL);
     return image
 }
@@ -478,11 +479,12 @@ ToolbarInventory.addImage = function(rect, name) {
 
 
 ToolbarInventory.updatePathLabel = function() {
-    log(" is updating?!")
     if (pathLabel != nil) {
-      log("updating label!")
-      log(userDefaults.myExportPath)
-      [pathLabel setStringValue:userDefaults.myExportPath];
+      var pathComps = userDefaults.myExportPath.split("/");
+      pathComps.pop();
+      log("RE ->>>>>>>")
+      var localLabel = pathComps.pop()
+      [pathLabel setStringValue:localLabel];
     }
 }
 
@@ -497,17 +499,17 @@ ToolbarInventory.createUIBar = function() {
     if(!UIBar){
         UIBar = NSPanel.alloc().init();
         UIBar.setStyleMask(NSTitledWindowMask + NSFullSizeContentViewWindowMask);
-        UIBar.setBackgroundColor(NSColor.colorWithRed_green_blue_alpha(0.15, 0.15, 0.15, 1));
+        UIBar.setBackgroundColor(NSColor.colorWithRed_green_blue_alpha(0.84, 0.84, 0.84, 1));
         UIBar.setTitleVisibility(NSWindowTitleHidden);
         UIBar.setTitlebarAppearsTransparent(true);
-        UIBar.setFrame_display(NSMakeRect(0, 0, 640, 70), false);
+        UIBar.setFrame_display(NSMakeRect(0, 0, 440, 70), false);
         UIBar.setMovableByWindowBackground(true);
         UIBar.setHasShadow(true);
         UIBar.setLevel(NSFloatingWindowLevel);
 
         var contentView = UIBar.contentView()
 
-        var closeButton = ToolbarInventory.addButton(NSMakeRect(20, 10, 30, 30), "close-control",
+        var closeButton = ToolbarInventory.addButton(NSMakeRect(418, 48, 12, 12), "close",
             function(sender){
                 coscript.setShouldKeepAround(false);
                 threadDictionary.removeObjectForKey(identifier);
@@ -515,85 +517,154 @@ ToolbarInventory.createUIBar = function() {
             }
         )
 
-        var topGuideB = ToolbarInventory.addButton( NSMakeRect(100, 10, 30, 30), "top-guide",
+        var topGuideB = ToolbarInventory.addButton( NSMakeRect(10, 6, 26, 26), "simulate",
             function(sender){
                 ToolbarInventory.updateContext();
                 runSimulateKeynote()
         })
-        var rightGuideB = ToolbarInventory.addButton( NSMakeRect(150, 10,30,30), "right-guide",
+        var rightGuideB = ToolbarInventory.addButton( NSMakeRect(68, 6, 26, 26), "generate",
             function(sender){
                 ToolbarInventory.updateContext();
                 runGenerateStates()
         })
-        var bottomGuideB = ToolbarInventory.addButton( NSMakeRect(200, 10,30,30), "bottom-guide",
+        var bottomGuideB = ToolbarInventory.addButton( NSMakeRect(102, 6, 26,26), "replicate",
             function(sender){
                 ToolbarInventory.updateContext();
                 runReplicateLayers()
         })
-        var leftGuideB = ToolbarInventory.addButton( NSMakeRect(250, 10,30,30), "left-guide",
+        var leftGuideB = ToolbarInventory.addButton( NSMakeRect(402, 48, 12, 12), "settings",
             function(sender){
                 ToolbarInventory.updateContext();
                 runSettings()
         })
-        var vCenterGuideB = ToolbarInventory.addButton( NSMakeRect(330, 10,30,30), "v-center-guide",
+        var vCenterGuideB = ToolbarInventory.addButton( NSMakeRect(26, 48,12,12), "add",
             function(sender){
                 ToolbarInventory.updateContext();
                 FramerInventory.runSelectProjectFolder()
                 ToolbarInventory.updatePathLabel()
         })
-        var hCenterGuideB = ToolbarInventory.addButton( NSMakeRect(380, 10,30,30), "h-center-guide",
+        var hCenterGuideB = ToolbarInventory.addButton( NSMakeRect(10, 48,12,12), "remove",
             function(sender){
                 ToolbarInventory.updateContext();
                 FramerInventory.runRemoveProjectFolder()
                 ToolbarInventory.updatePathLabel()
         })
-        var topBottomGuides = ToolbarInventory.addButton( NSMakeRect(460,10,30,30),"top-bottom-guides",
+        var topBottomGuides = ToolbarInventory.addButton( NSMakeRect(228, 6, 56, 26), "relative",
             function(sender){
                 ToolbarInventory.updateContext();
-                // currentSelection
+                // ToolbarInventory.nextLayerState(true)
+
         })
-        var rightLeftGuides = ToolbarInventory.addButton ( NSMakeRect(510, 10, 30,30), "right-left-guides",
+        var rightLeftGuides = ToolbarInventory.addButton ( NSMakeRect(292, 6, 26, 26), "density",
             function(sneder){
                 ToolbarInventory.updateContext();
+                // ToolbarInventory.nextLayerState(false)
         })
-        var removeAllGuidesB = ToolbarInventory.addButton( NSMakeRect(590, 10,30,30), "remove-all-guides",
+        var removeAllGuidesB = ToolbarInventory.addButton( NSMakeRect(326, 6, 104, 26), "device",
             function(sender){
                 ToolbarInventory.updateContext();
         })
 
-        var separate1 = ToolbarInventory.addImage( NSMakeRect(70, 10, 10, 30), "separate")
-        var separate2 = ToolbarInventory.addImage( NSMakeRect(300, 10, 10, 30), "separate")
-        var separate3 = ToolbarInventory.addImage( NSMakeRect(430, 10, 10, 30), "separate")
-        var separate4 = ToolbarInventory.addImage( NSMakeRect(560, 10, 10, 30), "separate")
+        // var separate1 = ToolbarInventory.addImage( NSMakeRect(70, 10, 10, 30), "separate")
+        // var separate2 = ToolbarInventory.addImage( NSMakeRect(300, 10, 10, 30), "separate")
+        // var separate3 = ToolbarInventory.addImage( NSMakeRect(430, 10, 10, 30), "separate")
+        // var separate4 = ToolbarInventory.addImage( NSMakeRect(560, 10, 10, 30), "separate")
+        var breaker = ToolbarInventory.addImage( NSMakeRect(10, 38, 420, 2), "breaker")
 
-        pathLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 50, 400, 30)];
+        pathLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(46, 47, 300, 20)];
         [pathLabel setEditable:false];
         [pathLabel setBordered:false];
-        [pathLabel setFont:[NSFont boldSystemFontOfSize:smallFontSize]];
+        // [pathLabel setFont:[NSFont boldSystemFontOfSize:smallFontSize]];
+        [pathLabel setFont:[NSFont fontWithName:@"System" size:12]];
         [pathLabel setTextColor:[NSColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:1]];
         [pathLabel setDrawsBackground:false];
-        [pathLabel setStringValue:userDefaults.myExportPath];
-        [pathLabel sizeToFit];
-        log("label DONE path-:P")
 
-        contentView.addSubview(closeButton);
-        contentView.addSubview(separate1);
+        [pathLabel setStringValue:defaultPathLabel];
+        [pathLabel sizeToFit];
+
+        // ToolbarInventory.updatePathLabel();
+
+        contentView.addSubview(breaker);
+
         contentView.addSubview(topGuideB);
         contentView.addSubview(rightGuideB);
         contentView.addSubview(bottomGuideB);
-        contentView.addSubview(leftGuideB);
-        contentView.addSubview(separate2);
+
+        // contentView.addSubview(separate2);
         contentView.addSubview(vCenterGuideB);
         contentView.addSubview(hCenterGuideB);
-        contentView.addSubview(separate3);
+        // contentView.addSubview(separate3);
         contentView.addSubview(topBottomGuides);
         contentView.addSubview(rightLeftGuides);
-        contentView.addSubview(separate4);
+        // contentView.addSubview(separate4);
         contentView.addSubview(removeAllGuidesB);
+
         contentView.addSubview(pathLabel);
+        contentView.addSubview(leftGuideB);
+        contentView.addSubview(closeButton);
 
         threadDictionary[identifier] = UIBar;
         UIBar.center();
         UIBar.makeKeyAndOrderFront(nil);
+
+        ToolbarInventory.updatePathLabel()
     }
 }
+
+
+//
+// ToolbarInventory.getForwardArtboards = function() {
+//     return [[currentDocument currentPage] artboards];
+// }
+//
+// ToolbarInventory.getBackwardArtboards = function() {
+//     return [[[[currentDocument currentPage] artboards] reverseObjectEnumerator] allObjects];
+// }
+//
+// ToolbarInventory.nextLayerState = function(isForwardOrder) {
+//   log("-> Next Layer")
+//   if ([currentSelection count] == 0) { return }
+//   var currentLayer = [currentSelection objectAtIndex: 0]
+//   var parentArtboard = TypeInventory.findParentArtboard(currentLayer)
+//   var parentName = [parentArtboard name]
+//
+//   var needNextArtboard = false
+//   var nextArtboard = nil
+//
+//   var baseArtboards = nil
+//   if (isForwardOrder) { baseArtboards = ToolbarInventory.getForwardArtboards() }
+//   else { baseArtboards = ToolbarInventory.getBackwardArtboards() }
+//
+//   var artboards = NSMutableArray.new()
+//   for (var i = 0; i < [baseArtboards count]; i++) {
+//       [artboards addObject:[baseArtboards objectAtIndex: i]]
+//   }
+//   for (var i = 0; i < [baseArtboards count]; i++) {
+//       [artboards addObject:[baseArtboards objectAtIndex: i]]
+//   }
+//
+//   for (var i = 0; i < artboards.count(); i++) {
+//      var artboard = [artboards objectAtIndex: i]
+//      if (needNextArtboard) {
+//         nextArtboard = artboard
+//         break
+//      }
+//
+//      if ([[parentArtboard objectID] isEqualToString:[artboard objectID]]) {
+//         needNextArtboard = true
+//      }
+//   }
+//
+//   var artboardLayers = nil
+//   if (nextArtboard) {
+//       FramerInventory.deselectLayers()
+//       artboardLayers = nextArtboard.children()
+//       for (var i = 0; i < artboardLayers.count(); i++) {
+//           var currentChild = [artboardLayers objectAtIndex: i]
+//           var currentChildName = [currentChild name]
+//           if ([currentChildName isEqualToString: [currentLayer name]]) {
+//               [currentChild select:true byExpandingSelection:true]
+//           }
+//       }
+//   }
+// }
