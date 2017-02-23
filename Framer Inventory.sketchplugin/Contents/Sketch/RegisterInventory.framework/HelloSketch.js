2b256a151dbdb284a3121a9d20f1e1d29fb7126f
Timur Nurutdinov <addleimb@gmail.com>
2017-02-23 23:40:33 +0300

    Added license support
    – Now you can add a license
    – Updated Settings to show license status
    
    // TODO
    – Remove license??
    – Add label for actions left to UI

---
+++ Framer Inventory.sketchplugin/Contents/Sketch/RegisterInventory.framework/HelloSketch.js
@@ -0,0 +1,20 @@
+var HelloSketch_FrameworkPath = HelloSketch_FrameworkPath || COScript.currentCOScript().env().scriptURL.path().stringByDeletingLastPathComponent();
+
+var HelloSketch_Log = HelloSketch_Log || log;
+
+(function() {
+ var mocha = Mocha.sharedRuntime();
+ var frameworkName = "RegisterInventory";
+ var directory = HelloSketch_FrameworkPath;
+ if (mocha.valueForKey(frameworkName)) {
+ HelloSketch_Log("😎 loadFramework: `" + frameworkName + "` already loaded.");
+ return true;
+ } else if ([mocha loadFrameworkWithName:frameworkName inDirectory:directory]) {
+ HelloSketch_Log("✅ loadFramework: `" + frameworkName + "` success!");
+ mocha.setValue_forKey_(true, frameworkName);
+ return true;
+ } else {
+ HelloSketch_Log("❌ loadFramework: `" + frameworkName + "` failed!: " + directory + ". Please define HelloSketch_FrameworkPath if you're trying to @import in a custom plugin");
+ return false;
+ }
+ })();
---
+++ Framer Inventory.sketchplugin/Contents/Sketch/RegisterInventory.framework/Versions/A/Headers/RegisterInventory.h
@@ -0,0 +1,19 @@
+//
+//  RegisterInventory.h
+//  RegisterInventory
+//
+//  Created by Timur Nurutdinov on 2/18/17.
+//  Copyright © 2017 Timur Nurutdinov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RegisterInventory.
+FOUNDATION_EXPORT double RegisterInventoryVersionNumber;
+
+//! Project version string for RegisterInventory.
+FOUNDATION_EXPORT const unsigned char RegisterInventoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegisterInventory/PublicHeader.h>
+
+
