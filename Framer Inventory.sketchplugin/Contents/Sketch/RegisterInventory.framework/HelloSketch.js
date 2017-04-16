d7f02efd81f2d562123952ae74582296e62badda
Timur Nurutdinov <addleimb@gmail.com>
2017-04-16 13:58:00 +0300

    Updated registry

--- Framer Inventory.sketchplugin/Contents/Sketch/RegisterInventory.framework/HelloSketch.js
+++ Framer Inventory.sketchplugin/Contents/Sketch/RegisterInventory.framework/HelloSketch.js
@@ -7,14 +7,14 @@ var HelloSketch_Log = HelloSketch_Log || log;
  var frameworkName = "RegisterInventory";
  var directory = HelloSketch_FrameworkPath;
  if (mocha.valueForKey(frameworkName)) {
- HelloSketch_Log("😎 loadFramework: `" + frameworkName + "` already loaded.");
+// HelloSketch_Log("😎 loadFramework: `" + frameworkName + "` already loaded.");
  return true;
  } else if ([mocha loadFrameworkWithName:frameworkName inDirectory:directory]) {
- HelloSketch_Log("✅ loadFramework: `" + frameworkName + "` success!");
+// HelloSketch_Log("✅ loadFramework: `" + frameworkName + "` success!");
  mocha.setValue_forKey_(true, frameworkName);
  return true;
  } else {
- HelloSketch_Log("❌ loadFramework: `" + frameworkName + "` failed!: " + directory + ". Please define HelloSketch_FrameworkPath if you're trying to @import in a custom plugin");
+// HelloSketch_Log("❌ loadFramework: `" + frameworkName + "` failed!: " + directory + ". Please define HelloSketch_FrameworkPath if you're trying to @import in a custom plugin");
  return false;
  }
  })();
